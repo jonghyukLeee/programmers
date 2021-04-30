a8744590e19088620d72a84db0a6823959f1fe1c
jonghyukleee <jh5253krkr@naver.com>
2021-04-30 19:54:45 +0900

    "Hash_Q2 commit"

---
+++ src/Hash/Q2.java
@@ -0,0 +1,12 @@
+package Hash;
+
+public class Q2 {
+    public static void main(String[] args) {
+
+    }
+        static boolean solution(String[] phone_book) {
+            boolean answer = true;
+            return answer;
+        }
+
+}
