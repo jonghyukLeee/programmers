8d91b5547adb97acfb85912f06ce130ca6c94027
jonghyukleee <jh5253krkr@naver.com>
2021-05-01 22:28:24 +0900

    "Hash_Q2 commit"

--- src/Hash/Q2.java
+++ src/Hash/Q2.java
@@ -1,12 +1,34 @@
 package Hash;
 
-public class Q2 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Q2 {
+    static String [] phone_book;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int t = Integer.parseInt(br.readLine());
+        phone_book = new String[t];
+        for(int i = 0; i < t; ++i)
+        {
+            phone_book[i] = st.nextToken();
+        }
+        solution(phone_book);
     }
         static boolean solution(String[] phone_book) {
-            boolean answer = true;
-            return answer;
+            for(int i = 0; i < phone_book.length; ++i)
+            {
+                for(int j = 0; j < phone_book.length; ++j)
+                {
+                    if(i == j) continue;
+                    if(phone_book[i].startsWith(phone_book[j])) return false;
+                }
+            }
+            return true;
         }
 
 }
