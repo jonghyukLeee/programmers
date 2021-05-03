0685880119f0a78787253bf52153c136de064762
jonghyukleee <jh5253krkr@naver.com>
2021-05-03 18:54:00 +0900

    "Hash_Q3 commit"

--- src/Hash/Q3.java
+++ src/Hash/Q3.java
@@ -1,11 +1,39 @@
 package Hash;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Q3 {
-    public static void main(String[] args) {
+    static String [][] clothes;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        clothes = new String[t][2];
+        for(int i = 0; i < t; ++i)
+        {
+            st = new StringTokenizer(br.readLine());
+            clothes[i][0] = st.nextToken();
+            clothes[i][1] = st.nextToken();
+        }
+        System.out.print(solution(clothes));
     }
+
+        static int solution(String[][] clothes) {
+            HashMap<String, Integer> hm = new HashMap();
+            int answer = 1;
+            for (int i = 0; i < clothes.length; i++) {
+                hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 1) + 1);
+            }
+
+            for (int i : hm.values()) {
+                System.out.printf("%d * %d = %d\n",answer,i,answer*i);
+                answer *= i;
+            }
+            return answer-1;
+        }
+
 }
