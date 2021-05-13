f745dec21a5edbe4ef5fe5f374d58aa125fffa97
jonghyukleee <jh5253krkr@naver.com>
2021-05-13 17:42:59 +0900

    "SQ_3 commit"

--- src/StackQueue/Q3.java
+++ src/StackQueue/Q3.java
@@ -1,7 +1,38 @@
 package StackQueue;
 
-public class Q3 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+class Truck
+{
+    int weight, time = 0;
+    public Truck(int weight)
+    {
+        this.weight = weight;
+    }
+}
+public class Q3 {
+    static int [] trucks;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(br.readLine());
+        int weight_lim = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+        trucks = new int[t];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < t; ++i)
+        {
+            trucks[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.print(solution(length,weight_lim,trucks));
+    }
+    static int solution(int bridge_length, int weight, int[] truck_weights) {
+        int answer = 0;
+        Queue<Truck> q = new LinkedList<>();
+        return answer;
     }
 }
