c305f21ce5227899357610eb50b4ea44aa677430
jonghyukleee <jh5253krkr@naver.com>
2021-05-24 18:53:21 +0900

    "Heap_Q3 commit"

--- src/Heap/Q3.java
+++ src/Heap/Q3.java
@@ -7,9 +7,9 @@ import java.util.*;
 
 public class Q3 {
     static String [] operations;
+    static StringTokenizer st;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         int t = Integer.parseInt(br.readLine());
         operations = new String[t];
         for(int i = 0; i < t; ++i)
@@ -22,12 +22,50 @@ public class Q3 {
         }
     }
     static int[] solution(String[] operations) {
-        int[] answer = {};
+        int[] answer = new int [2];
         PriorityQueue<Integer> minQ = new PriorityQueue<>();
         PriorityQueue<Integer> maxQ = new PriorityQueue<Integer>(Collections.reverseOrder());
         for(int i = 0; i < operations.length; ++i)
         {
+            st = new StringTokenizer(operations[i]);
+            String op = st.nextToken();
+            int tmp = Integer.parseInt(st.nextToken());
 
+            switch(op)
+            {
+                case "I" :
+                {
+                    minQ.add(tmp);
+                    maxQ.add(tmp);
+                    break;
+                }
+                case "D" :
+                {
+                    if(minQ.isEmpty()) break;
+                    if(tmp > 0)
+                    {
+                        Object del = maxQ.poll();
+                        minQ.remove(del);
+                        break;
+                    }
+                    else
+                    {
+                        Object del = minQ.poll();
+                        maxQ.remove(del);
+                        break;
+                    }
+                }
+            }
+        }
+        if(minQ.isEmpty())
+        {
+            answer[0] = 0;
+            answer[1] = 0;
+        }
+        else
+        {
+            answer[0] = maxQ.poll();
+            answer[1] = minQ.poll();
         }
         return answer;
     }
