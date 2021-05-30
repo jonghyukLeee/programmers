e846ad4bc2ff4c2499c9bbf0a9c264c5d5ea8f44
jonghyukleee <jh5253krkr@naver.com>
2021-05-30 18:52:37 +0900

    "DP_Q1 commit"

--- src/DP/Q1.java
+++ src/DP/Q1.java
@@ -3,25 +3,69 @@ package DP;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+class Num
+{
+    int cnt, val;
+    public Num(int cnt, int val)
+    {
+        this.cnt = cnt;
+        this.val = val;
+    }
+}
 public class Q1 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        int n =0 , number =0;
-        for(int i =0; i < 2; ++i)
-        {
-            n = Integer.parseInt(st.nextToken());
-            number = Integer.parseInt(st.nextToken());
-        }
+        int n = Integer.parseInt(st.nextToken());
+        int number = Integer.parseInt(st.nextToken());
         System.out.print(solution(n,number));
 
     }
     static int solution(int N, int number) {
-        int answer = 0;
-
+        int answer = 0; // 5 12
+        Queue<Num> q = new LinkedList<>();
+        q.add(new Num(0,N));
+        while(!q.isEmpty())
+        {
+            Num tmp = q.poll();
+            int val = tmp.val; //5
+            for(int i = 0; i < 4; ++i) // + - / *
+            {
+                switch(i)
+                {
+                    case 0:
+                    {
+                        q.add(new Num(tmp.cnt+1,val+N));
+                        break;
+                    }
+                    case 1:
+                    {
+                        q.add(new Num(tmp.cnt+1,val-N));
+                        break;
+                    }
+                    case 2:
+                    {
+                        q.add(new Num(tmp.cnt+1,val/N));
+                        break;
+                    }
+                    case 3:
+                    {
+                        q.add(new Num(tmp.cnt+1,val*N));
+                        break;
+                    }
+                }
+            }
+        }
         return answer;
+    }
+    static void bfs()
+    {
+
     }
 }
