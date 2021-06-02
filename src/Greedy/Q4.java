7034dc7620782047e210b5211e4400e349cb262d
jonghyukleee <jh5253krkr@naver.com>
2021-06-02 23:54:50 +0900

    "Greedy_Q4 commit"

--- src/Greedy/Q4.java
+++ src/Greedy/Q4.java
@@ -9,6 +9,7 @@ public class Q4 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        costs = new int[n][n];
         for(int i = 0; i < n; ++i)
         {
 
@@ -16,6 +17,7 @@ public class Q4 {
     }
     static int solution(int n, int[][] costs) {
         int answer = 0;
+        int cost = 0;
         return answer;
     }
 }
