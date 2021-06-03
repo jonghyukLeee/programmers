2f7f57572e317325c4f346e6931b2ab3974d6087
jonghyukleee <jh5253krkr@naver.com>
2021-06-03 20:49:29 +0900

    "Greedy_Q4 commit"

--- src/Greedy/Q4.java
+++ src/Greedy/Q4.java
@@ -3,16 +3,23 @@ package Greedy;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Q4 {
     static int [][] costs;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
         int n = Integer.parseInt(br.readLine());
         costs = new int[n][n];
-        for(int i = 0; i < n; ++i)
+        StringTokenizer st;
+        for(int i = 0; i < t; ++i)
         {
-
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < 3; ++j)
+            {
+                costs[i][j] = Integer.parseInt(st.nextToken());
+            }
         }
     }
     static int solution(int n, int[][] costs) {
