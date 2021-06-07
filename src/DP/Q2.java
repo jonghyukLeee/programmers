06129d685a8cd77c8478bb997a41bab05b7978bb
jonghyukleee <jh5253krkr@naver.com>
2021-06-07 22:44:41 +0900

    "DP_Q2 commit"

--- src/DP/Q2.java
+++ src/DP/Q2.java
@@ -1,7 +1,21 @@
 package DP;
 
-public class Q2 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class Q2 {
+    static int [][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        dp = new int[t][t];
+        for(int i = 0; i < t; ++i)
+        {
+            st = new StringTokenizer(br.readLine());
+
+        }
     }
 }
