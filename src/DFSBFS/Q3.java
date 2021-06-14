f69a3a5da05091d4b9ab12a7df6f2252f5609e31
jonghyukleee <jh5253krkr@naver.com>
2021-06-15 01:21:40 +0900

    "DFS_Q3 commit"

---
+++ src/DFSBFS/Q3.java
@@ -0,0 +1,30 @@
+package DFSBFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q3 {
+    static String begin,target;
+    static String [] words;
+    static int len;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        begin = br.readLine();
+        target = br.readLine();
+        len = begin.length();
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0 ; i < t; ++i)
+        {
+            words[i] = st.nextToken();
+        }
+        System.out.print(solution(begin,target,words));
+    }
+    static int solution(String begin, String target, String[] words) {
+        int answer = 0;
+
+        return answer;
+    }
+}
