d490049f98b2c5b0ba15a6baa5bf40933c022d7c
jonghyukleee <jh5253krkr@naver.com>
2021-08-28 11:04:03 +0900

    "Q3 commit"

--- src/Kakao2021/Q3.java
+++ src/Kakao2021/Q3.java
@@ -1,7 +1,12 @@
 package Kakao2021;
 
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class Q3 {
+    static HashMap<String, ArrayList<Integer>> map;
     public static void main(String[] args) {
         String [] info = {"java backend junior pizza 150","python frontend senior chicken 210",
                 "python frontend senior chicken 150","cpp backend senior pizza 260",
@@ -13,8 +18,52 @@ public class Q3 {
         for(int i : solution(info,query)) System.out.print(i+" ");
     }
     static int[] solution(String[] info, String[] query) {
-        int[] answer = {};
+        int[] answer = new int[query.length];
+        map = new HashMap<>();
+        for(String in : info)
+        {
+            String [] infoArr = in.split(" ");
+            comb("",0,infoArr);
+        }
 
+        for(String key : map.keySet()) Collections.sort(map.get(key));
+        int queryIdx = 0;
+        for(String q : query)
+        {
+            String str = q.replace(" and ","");
+            String [] tmp = str.split(" ");
+            answer[queryIdx++] = binarySearch(tmp[0],Integer.parseInt(tmp[1]));
+        }
         return answer;
+    }
+    static void comb(String str,int depth,String [] arr) {
+        if (depth == 4) {
+            int score = Integer.parseInt(arr[4]);
+            if (map.containsKey(str)) map.get(str).add(score);
+            else {
+                ArrayList<Integer> tmp = new ArrayList<>();
+                tmp.add(score);
+                map.put(str, tmp);
+            }
+            return;
+        }
+
+        comb(str + "-", depth + 1, arr);
+        comb(str + arr[depth], depth + 1, arr);
+    }
+
+    static int binarySearch(String query, int score)
+    {
+        if(!map.containsKey(query)) return 0;
+        ArrayList<Integer> tmpList = map.get(query);
+        int start = 0, end = tmpList.size()-1;
+        while(start <= end)
+        {
+            int mid = (start + end) / 2;
+
+            if(score > tmpList.get(mid)) start = mid + 1;
+            else end = mid - 1;
+        }
+        return tmpList.size() - start;
     }
 }
