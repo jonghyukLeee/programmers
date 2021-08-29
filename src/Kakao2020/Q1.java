9b9064cced629d47382f5c1bddc2bb43d6c28d02
jonghyukleee <jh5253krkr@naver.com>
2021-08-29 23:46:38 +0900

    "Q1 commit"

--- src/Kakao2020/Q1.java
+++ src/Kakao2020/Q1.java
@@ -1,54 +1,54 @@
 package Kakao2020;
 
+import java.util.ArrayList;
+
 public class Q1 {
     public static void main(String[] args) {
-        String s = "aabbaccc";
+        String s = "a";
         System.out.print(solution(s));
 
     }
     static int solution(String s) {
         int answer = Integer.MAX_VALUE;
+        if(s.length() < 2) return 1;
         for(int i = 1; i <= s.length()/2; ++i)
         {
             StringBuilder sb = new StringBuilder();
-            System.out.println(i);
             answer = Math.min(compress(s,sb,i),answer);
         }
         return answer;
     }
-    static int compress(String origin, StringBuilder cur,int len) // 원본문자열, 압축문자열, 길이
+    static int compress(String origin, StringBuilder cur,int len)
     {
-        String pre = origin.charAt(0)+"";
-        int cnt = 0;
+        ArrayList<String> al = new ArrayList<>();
         for(int i = 0; i < origin.length(); i+=len)
         {
-            if(i+len > origin.length())
-            {
-                cur.append(origin.substring(i,origin.length()));
-                break;
-            }
-            String cut = origin.substring(i,i+len);
-            System.out.printf("cur %d ~ %d => %s\n",i,i+len,cut);
-            if(pre.equals(cut))
-            {
-                cnt++;
-                if(i+len == origin.length()) cur.append(cnt).append(pre);
-            }
+            if(i+len > origin.length()) al.add(origin.substring(i));
+            else al.add(origin.substring(i,i+len));
+        }
+        String pre = al.get(0);
+        int cnt = 1;
+        for(int i = 1; i < al.size(); ++i)
+        {
+            if(pre.equals(al.get(i))) cnt++;
             else
             {
                 if(cnt > 1)
                 {
                     cur.append(cnt).append(pre);
+                    pre = al.get(i);
+                    cnt = 1;
                 }
                 else
                 {
                     cur.append(pre);
+                    pre = al.get(i);
                 }
-                pre = cut;
-                cnt = 1;
             }
         }
-        System.out.println(cur.toString());
+        if(cnt > 1) cur.append(cnt).append(pre);
+        else cur.append(pre);
+
         return cur.length();
     }
 }
