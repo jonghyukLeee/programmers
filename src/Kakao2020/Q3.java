3e6e84ca7153620163a210e444460d1bb846755d
jonghyukleee <jh5253krkr@naver.com>
2021-09-02 23:44:14 +0900

    "Q3 commit"

--- src/Kakao2020/Q3.java
+++ src/Kakao2020/Q3.java
@@ -1,71 +1,74 @@
 package Kakao2020;
 
 public class Q3 {
-    static int n;
+    static int n,cnt,keyLen;
     static int [][] map;
-    static int maxI = -1, maxJ = -1;
     public static void main(String[] args) {
-
+        int [][] key = {{0, 0, 0}, {0, 0, 1}, {1, 1, 0}};
+        int [][] lock = {{1, 1, 1,1},{0,1, 1, 1},{0,1, 1, 1},{1,0,1,1}};
+        System.out.print(solution(key,lock));
     }
     static boolean solution(int[][] key, int[][] lock) {
         boolean answer = false;
-        n = lock.length;
-        int mapLen = (3 * n) - 2;
+        keyLen = key.length;
+        n = 2*(key.length-1);
+        cnt = 0;
+        int mapLen = lock.length + n;
         map = new int[mapLen][mapLen];
-
-
-        for(int i = 0; i < n; ++i)
+        for(int i = 0; i < lock.length; ++i)
         {
-            for(int j = 0; j < n; ++j)
+            for(int j = 0; j < lock.length; ++j)
             {
-                if(lock[i][j] == 1) map[i+n-1][j+n-1] = 1;
-                else
-                {
-                    if(maxI < i+n-1) maxI = i+n-1;
-                    if(maxJ < j+n-1) maxJ = j+n-1;
-                }
+                if(lock[i][j] == 1) map[i+key.length-1][j+key.length-1] = 1;
+                else cnt++;
             }
         }
 
+        int [][] tmpKey = new int[key.length][key.length];
+        for(int i = 0; i < key.length; ++i)
+        {
+            System.arraycopy(key[i],0,tmpKey[i],0,key[i].length);
+        }
         for(int t = 0; t < 4; ++t)
         {
-            int [][] tmpKey = rotate(key);
             if(unlock(tmpKey))
             {
                 answer = true;
                 break;
             }
+            tmpKey = rotate(tmpKey);
         }
         return answer;
     }
     static boolean unlock(int [][] key)
     {
-        for(int i = 0; i < map.length-1; ++i)
+        int correct;
+        for(int i = 0; i <= map.length-keyLen; ++i)
         {
-            if((i+ key.length) <= maxI) continue;
-            for(int j = 0; j < map.length-1; ++j)
+            for(int j = 0; j <= map.length-keyLen; ++j)
             {
-                if((j + key.length) <= maxJ) continue;
-                for(int k = i; k < i+key.length; ++k)
+                correct = 0;
+                next : for(int k = i; k < i+keyLen; ++k)
                 {
-                    for(int l = j; l < j+key.length; ++l)
+                    for(int l = j; l < j+keyLen; ++l)
                     {
                         if(inArr(k,l))
                         {
                             if(map[k][l] == 0)
                             {
-                                if(key[k-i][l-j] == 0) return false;
+                                if(key[k-i][l-j] == 1) correct++;
                             }
                             else
                             {
-                                if(key[k-i][l-j] == 1) return false;
+                                if(key[k-i][l-j] == 1) continue next;
                             }
                         }
                     }
+                    if(correct == cnt) return true;
                 }
             }
         }
-        return true;
+        return false;
     }
     static int [][] rotate(int [][] key)
     {
@@ -83,6 +86,6 @@ public class Q3 {
     }
     static boolean inArr(int i, int j)
     {
-        return i <= n-1 || i > n+2 || j <= n-1 || j > n+2;
+        return i >= keyLen-1 && i <= map.length-keyLen && j >= keyLen-1 && j <= map.length-keyLen;
     }
 }
