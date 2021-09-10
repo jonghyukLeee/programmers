0fb36a4075ec1418c401b1a11adc0f9836a97e2a
jonghyukleee <jh5253krkr@naver.com>
2021-09-10 18:51:43 +0900

    "Kakao2019_Q3 commit"

--- src/Kakao2019/Q3.java
+++ src/Kakao2019/Q3.java
@@ -4,38 +4,37 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Q3 {
-        public int solution(String[][] relation) {
+    public static void main(String[] args) {
+        String [][] str = {{"100","ryan","music","2"}
+                ,{"200","apeach","math","2"},
+                {"300","tube","computer","3"},
+                {"400","con","computer","4"},
+                {"500","muzi","music","3"},
+                {"600","apeach","music","2"}};
+        System.out.print(solution(str));
+    }
+        static public int solution(String[][] relation) {
             int answer = 0;
             ArrayList<ArrayList<String>> map = new ArrayList<>();
 
             for(int i = 0; i < relation[0].length; ++i)
             {
                 map.add(new ArrayList<>());
-                for(String s : relation[i]) map.get(i).add(s);
+                for(int j = 0; j < relation.length; ++j)
+                {
+                    map.get(i).add(relation[j][i]);
+                }
                 Collections.sort(map.get(i));
             }
-
+            for(ArrayList<String> al : map)
+            {
+                for(String s : al) System.out.print(s+" ");
+                System.out.println();
+            }
             String tmp = "";
             boolean isUnique = false;
             boolean [] isVis = new boolean[relation[0].length];
-            next : for(int i = 0; i < map.size(); ++i)
-            {
-                isUnique = false;
-                for(String s : map.get(i))
-                {
-                    if(!tmp.equals(s))
-                    {
-                        tmp = s;
-                    }
-                    else continue next;
-                    isUnique = true;
-                }
-                if(isUnique)
-                {
-                    isVis[i] = true;
-                    answer++;
-                }
-            }
+
 
             return answer;
         }
