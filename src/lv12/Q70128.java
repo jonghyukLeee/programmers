8642f5d77fd35745c9183042429fff9d89f0c093
jonghyukleee <jh5253krkr@naver.com>
2022-05-19 14:02:31 +0900

    "내적"

---
+++ src/lv12/Q70128.java
@@ -0,0 +1,14 @@
+package lv12;
+
+public class Q70128 {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        int len = a.length;
+
+        for(int i = 0; i < len; i++)
+        {
+            answer += (a[i] * b[i]);
+        }
+        return answer;
+    }
+}
