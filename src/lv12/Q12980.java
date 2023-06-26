62e182ad5e71633a905b9d96b3f31725b983a692
jonghyukleee <jh5253krkr@naver.com>
2023-06-26 23:02:51 +0900

    Q12980 점프와 순간이동

---
+++ src/lv12/Q12980.java
@@ -0,0 +1,19 @@
+package lv12;
+
+public class Q12980 {
+    public static void main(String[] args) {
+
+    }
+    static int solution(int n) {
+        int answer = 0;
+
+        while (n > 0) {
+            if (n % 2 == 0) n /= 2;
+            else {
+                n--;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
